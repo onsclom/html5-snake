b09466550ced7d9037b665e866a0fe95c5c68443
Gabriel Sroka <gabrielsroka@gmail.com>
2022-12-26 12:51:28 -0800

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,16 +1,16 @@
 const GRID_SIZE = 20;
 const FPS = 12;
-const v2d = (x, y) => ({ x, y, toString: () => `${x}, ${y}` });
+const v2d = (x, y) => ({ x, y, toString: () => `${x}, ${y}`, on: (v) => x === v.x && y === v.y });
 const randInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 const randPos = () => v2d(randInt(0, GRID_SIZE), randInt(0, GRID_SIZE));
 
-let snake = [v2d(2, 2)];
+const snake = [v2d(2, 2)];
 let size = 1;
 let dir = v2d(0, 0);
 let apple = randPos();
-let moveBuffer = [];
-let canvas = document.querySelector("#canvas");
-let interval = setInterval(gameLoop, 1000 / FPS);
+const moveBuffer = [];
+const canvas = document.querySelector("#canvas");
+const interval = setInterval(gameLoop, 1000 / FPS);
 
 canvas.width = GRID_SIZE;
 canvas.height = GRID_SIZE;
@@ -18,14 +18,16 @@ window.onkeydown = (event) => changeDir(event.key);
 
 function gameLoop() {
   if (moveBuffer.length) dir = moveBuffer.shift();
-  snake = [v2d(snake[0].x + dir.x, snake[0].y + dir.y), ...snake];
-  const snakeOnApple = String(snake[0]) === String(apple);
-  size = snakeOnApple ? size + 1 : size;
-  snake = snake.slice(0, size);
+  snake.unshift(v2d(snake[0].x + dir.x, snake[0].y + dir.y));
+  if (snake[0].on(apple)) {
+    size++;
+    apple = randPos();
+  } else {
+    snake.pop();
+  }
   const snakeOnSnake = new Set(snake.map(String)).size !== snake.length;
-  const inBounds = (x) => x >= 0 && x < GRID_SIZE;
+  const inBounds = (c) => c >= 0 && c < GRID_SIZE;
   const snakeInBounds = inBounds(snake[0].x) && inBounds(snake[0].y);
-  apple = snakeOnApple ? randPos() : apple;
   if (snakeOnSnake || !snakeInBounds) {
     clearInterval(interval);
     alert(`Game over. Your snake was ${size} long!`);
@@ -49,6 +51,6 @@ function changeDir(arrow) {
   };
   const newDir = dirs[arrow] || dir;
   const prevDir = moveBuffer.at(-1) || dir;
-  const illegalDir = prevDir.x === -newDir.x && prevDir.y === -newDir.y;
-  if (!illegalDir) moveBuffer = [...moveBuffer, newDir];
+  const illegalDir = prevDir.on(v2d(-newDir.x, -newDir.y));
+  if (!illegalDir) moveBuffer.push(newDir);
 }
